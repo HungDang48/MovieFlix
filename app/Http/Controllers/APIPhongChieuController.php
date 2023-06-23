e79756406e1773da8ed3de63335d536181f5d32f
Quoc Huy <vodinhquochuy1511@gmail.com>
2023-06-23 14:47:38 +0700

    phongchieu

--- app/Http/Controllers/APIPhongChieuController.php
+++ app/Http/Controllers/APIPhongChieuController.php
@@ -27,4 +27,60 @@ class APIPhongChieuController extends Controller
             'data'    => $data,
         ]);
     }
+
+    public function status(Request $request)
+    {
+        $phong   = PhongChieu::find($request->id);
+        if($phong) {
+            if($phong->tinh_trang == 1) {
+                $phong->tinh_trang = 0;
+            } else {
+                $phong->tinh_trang = 1;
+            }
+            $phong->save();
+
+            return response()->json([
+                'status'    => 1,
+                'message'   => 'Đã cập nhật trạng thái!'
+            ]);
+        } else {
+            return response()->json([
+                'status'    => 0,
+                'message'   => 'Phòng không tồn tại!'
+            ]);
+        }
+    }
+
+    public function info(Request $request)
+    {
+        $phong   = PhongChieu::find($request->id);
+        if($phong) {
+            return response()->json([
+                'status'    => 1,
+                'data'      => $phong
+            ]);
+        } else {
+            return response()->json([
+                'status'    => 0,
+                'message'   => 'Phòng không tồn tại!'
+            ]);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        $phong   = PhongChieu::find($request->id);
+        if($phong) {
+            $phong->delete();
+            return response()->json([
+                'status'    => 1,
+                'message'   => 'Đã xóa phòng thành công!'
+            ]);
+        } else {
+            return response()->json([
+                'status'    => 0,
+                'message'   => 'Phòng không tồn tại!'
+            ]);
+        }
+    }
 }
